66bf8dc8c3993e251779059f510cfe5c23dff399
signor1 <emmanuelomemgboji@gmail.com>
2025-03-04 19:53:50 +0100

    feat: added contract

---
+++ src/PoolPlayHook.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+
+contract PoolPlayHook is BaseHook {}
