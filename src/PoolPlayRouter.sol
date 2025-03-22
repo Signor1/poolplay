7f9d0a835f06dc8c71861e49ecb88fa389a72a1a
PhantomOz <faniogor@gmail.com>
2025-03-22 05:01:08 +0100

    This adds constructor for manager and pool

--- src/PoolPlayRouter.sol
+++ src/PoolPlayRouter.sol
@@ -1,4 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-contract PoolPlayRouter {}
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+
+contract PoolPlayRouter is Ownable {
+    IPoolManager public immutable manager;
+    address public immutable hook;
+
+    constructor(address _manager, address _hook) Ownable(msg.sender) {
+        manager = IPoolManager(_manager);
+        hook = _hook;
+    }
+}
